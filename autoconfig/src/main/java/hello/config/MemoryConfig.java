ee55dc1aaf7d90a6a4c670f3643797f419c71292
OllyDI <choiseungho11@hanmail.net>
2026-09-11 14:06:24 +0900

    26.09.11

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,16 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@Conditional(MemoryCondition.class) // 반환 값이 true 일 때만 스프링 빈에 등록
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 위의 클래스 기능과 동일
 public class MemoryConfig {
 
     @Bean
